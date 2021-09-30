a6b7f2fd23e194b78c4b24a90e7a7f1c3eb1dc36
berkangzgn <bgezgin@protonmail.com>
2021-09-30 17:03:22 +0300

    Notes listed

---
+++ ClassLibrary2/NoteManager.cs
@@ -0,0 +1,20 @@
+﻿using Evernote.DataAccessLayer.EntityFramework;
+using Evernote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evernote.BusinessLayer
+{
+    public class NoteManager
+    {
+        private Repository<EvernoteNote> repo_note = new Repository<EvernoteNote>();
+
+        public List<EvernoteNote> GetAllNote()
+        {
+            return repo_note.List();
+        }
+    }
+}
